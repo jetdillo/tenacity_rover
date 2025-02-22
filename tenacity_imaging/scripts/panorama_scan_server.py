34f77174efd1fda5c8ed64804f54c1d4533d5905
Jet Dillo <armadilo@gothpunk.com>
2025-02-21 19:12:17 -0800

    images are now dumped to separate rgb vs. depth subdirs

--- tenacity_imaging/scripts/panorama_scan_server.py
+++ tenacity_imaging/scripts/panorama_scan_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import rospy
 import actionlib
 import os
@@ -10,14 +11,18 @@ from std_msgs.msg import Float64
 from cv_bridge import CvBridge
 import cv2
 from tenacity_imaging.msg import PanoramaScanAction, PanoramaScanResult
+from dynamixel_msgs.msg import JointState as JointState_DM
+
 
 class PanoramaScanActionServer:
     def __init__(self):
         self.server = actionlib.SimpleActionServer('panorama_scan', PanoramaScanAction, self.execute, False)
         self.server.start()
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/rgb_stereo_publisher/color/image', Image, self.image_callback)
-        self.depth_sub = rospy.Subscriber('/rgb_stereo_publisher/stereo/depth', Image, self.depth_callback)
+        self.image_sub = rospy.Subscriber('/rgb_stereo_publisher/color/image', Image, self.image_cb)
+        self.depth_sub = rospy.Subscriber('/rgb_stereo_publisher/stereo/depth', Image, self.depth_cb)
+        self.pan_sub = rospy.Subscriber('/mastcam_pan_controller/state', JointState_DM, self.pan_cb)
+        self.tilt_sub = rospy.Subscriber('/mastcam_tilt_controller/state', JointState_DM, self.tilt_cb)
         self.pan_pub = rospy.Publisher('/mastcam_pan_controller/command', Float64, queue_size=10)
         self.tilt_pub = rospy.Publisher('/mastcam_tilt_controller/command', Float64, queue_size=10)
         self.current_image = None
@@ -25,74 +30,94 @@ class PanoramaScanActionServer:
         self.image_ready = False
         self.depth_ready = False
 
-    def image_callback(self, msg):
-        self.current_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.pan_pos = 0.0
+        self.tilt_pos = 0.0
+
+    def image_cb(self, img):
+        self.current_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         self.image_ready = True
 
-    def depth_callback(self, msg):
-        self.current_depth = self.bridge.imgmsg_to_cv2(msg, "passthrough")
+    def depth_cb(self, img):
+        self.current_depth = self.bridge.imgmsg_to_cv2(img, "passthrough")
         self.depth_ready = True
+
+    def pan_cb(self, data):
+        self.pan_pos=data.current_pos
+
+    def tilt_cb(self, data):
+        self.tilt_pos=data.current_pos
 
     def execute(self, goal):
         # Create a directory for the scan
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         #scan_dir = f"/tmp/tenacity_scan_{timestamp}"
         scan_dir = "/tmp/tenacity_scan_"+timestamp
+        rgb_dir = scan_dir+"/rgb"
+        depth_dir = scan_dir+"/depth"
         #os.makedirs(scan_dir, exist_ok=True)
         if not os.path.exists(scan_dir):
-            os.makedirs(scan_dir)
+            os.makedirs(rgb_dir)
+            os.makedirs(depth_dir)
 
         # Initialize tilt position
         tilt_angle = 0.0
-        tilt_increment = 30.0
+        tilt_increment = rospy.get_param("/panorama_scan_server/tilt_increment")
         tilt_direction = 1  # 1 for positive, -1 for negative
+ 
+        tilt_points = rospy.get_param("/panorama_scan_server/tilt_points")
+
+        # Initialize pan position 
+        pan_angle = 0.0
+        pan_increment = rospy.get_param("/panorama_scan_server/pan_increment")
+        pan_direction = 1
+
+        rospy.loginfo("Starting panorama capture")
+        rospy.loginfo("pan_angle:%f tilt_angle:%f",self.pan_pos,self.tilt_pos)
 
         while True:
             # Set tilt position
             self.tilt_pub.publish(Float64(tilt_angle))
-            rospy.sleep(2)  # Wait for the tilt servo to reach the position
+            rospy.sleep(3)  # Wait for the tilt servo to reach the position
 
             # Initialize pan position
-            pan_angle = 90.0
+            pan_angle = (90.0 * math.pi) / 180 
             self.pan_pub.publish(Float64(pan_angle))
-            rospy.sleep(2)  # Wait for the pan servo to reach the initial position
+            rospy.sleep(3)  # Wait for the pan servo to reach the initial position
 
-            while pan_angle >= -90.0:
-                # Capture image and depth
-                self.image_ready = False
-                self.depth_ready = False
-                while not self.image_ready or not self.depth_ready:
-                    rospy.sleep(0.1)
-
-                # Save image and depth
-                image_filename = scan_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+"_"+str(uuid.uuid4())+".jpg"
-
-                depth_filename = scan_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+"_"+str(uuid.uuid4())+".png"
-                cv2.imwrite(image_filename, self.current_image)
-                cv2.imwrite(depth_filename, self.current_depth)
-
-                # Move to the next pan position
-                pan_angle -= 30.0
+            for tilt_angle in tilt_points:
+                self.tilt_pub.publish(Float64(tilt_angle))
+                pan_angle = (90.0 * math.pi) / 180 
                 self.pan_pub.publish(Float64(pan_angle))
-                rospy.sleep(2)  # Wait for the servo to reach the next position
-
+                rospy.sleep(3)  # Wait for the pan servo to reach the initial position
+                
+                while pan_angle >= -1.57:
+                    rospy.loginfo("PAN ACTION - MOVING TO pan_angle:%f tilt_angle:%f",self.pan_pos,self.tilt_pos)
+                    # Capture image and depth
+                    self.image_ready = False
+                    self.depth_ready = False
+                    while not self.image_ready or not self.depth_ready:
+                        rospy.sleep(1)
+                        rospy.loginfo("image_ready:%s depth_ready: %s",self.image_ready,self.depth_ready)
+                    
+                    rospy.loginfo("DUMPING FRAMES")
+                    # Save image and depth
+                    #image_filename = rgb_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+"_"+str(uuid.uuid4())+".jpg"
+                    image_filename = rgb_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+str(self.pan_pos)+"_"+str(self.tilt_pos)+".jpg"
+    
+                    #depth_filename = scan_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+"_"+str(uuid.uuid4())+".png"
+                    depth_filename = depth_dir+"/"+datetime.now().strftime('%Y%m%d_%H%M%S')+"_"+str(self.pan_pos)+"_"+str(self.tilt_pos)+".png"
+                    cv2.imwrite(image_filename, self.current_image)
+                    cv2.imwrite(depth_filename, self.current_depth)
+    
+                    # Move to the next pan position
+                    pan_angle -= pan_increment
+                    self.pan_pub.publish(Float64(pan_angle))
+                    rospy.sleep(5)  # Wait for the servo to reach the next position
+    
             # Wait 5 seconds at -90 degrees pan
-            rospy.sleep(5)
+                rospy.sleep(5)
 
-            # Return pan to 0 degrees
-            self.pan_pub.publish(Float64(0.0))
-            rospy.sleep(2)
-
-            # Update tilt angle
-            if tilt_direction == 1:
-                tilt_angle += tilt_increment
-                if tilt_angle >= 90.0:
-                    tilt_direction = -1
-                    tilt_angle = -30.0  # Start moving in the negative direction
-            else:
-                tilt_angle -= tilt_increment
-                if tilt_angle < -90.0:
-                    break  # Exit the loop when tilt reaches -90 degrees
+            break  # Exit the loop when tilt reaches -90 degrees
 
         # Return the result
         result = PanoramaScanResult()
